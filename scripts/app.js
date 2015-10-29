a308d3be7964223cf1dbfb39eac66ab40fb21a65
Eugenio Mendoza <eu.mendoza@gmail.com>
2015-10-29 17:39:54 +0100

    Create app.js file, main control

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+(function(document) {
+	'use strict';
+
+	var app = document.querySelector('#app');
+	app.titleApp = 'Movies';
+	app.filmTab = 0;
+
+	app.scrollPageToTop = function() {
+		app.$.headerPanelMain.scrollToTop(true);
+	};
+
+	// Close drawer after menu item is selected if drawerPanel is narrow
+	app.onDataRouteClick = function() {
+		var drawerPanel = Polymer.dom(document).querySelector('#paperDrawerPanel');
+		if (drawerPanel.narrow) {
+			drawerPanel.closeDrawer();
+		}
+	};
+
+	window.addEventListener('WebComponentsReady', function() {
+		// imports are loaded and elements have been registered
+	});
+
+})(document);
